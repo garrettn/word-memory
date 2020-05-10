3e6e9f44d635f175f432e82bc60d42d52acd5b7d
Garrett Nay <g@garrettnay.com>
2020-05-09 21:11:11 -0600

    Enable flipping words

--- src/PlayGame/PlayGame.js
+++ src/PlayGame/PlayGame.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WordCard from '../WordCard'
 
 const words = ['foo', 'bar', 'baz', 'qux']
 
 function PlayGame() {
+  const [flippedWords, updateFlippedWords] = useState([])
   return (
     <>
       <h1>Play Game!</h1>
       <>
         {words.map((word) => (
-          <WordCard key={word} word={word} />
+          <WordCard
+            key={word}
+            word={word}
+            isFlipped={flippedWords.includes(word)}
+            onFlip={() => updateFlippedWords([...flippedWords, word])}
+          />
         ))}
       </>
     </>
