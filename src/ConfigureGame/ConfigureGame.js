371b344c8e78bef43c88edee4f111482c8fbcb8e
Garrett Nay <g@garrettnay.com>
2020-05-15 14:11:58 -0600

    Allow setting the list of words

--- src/ConfigureGame/ConfigureGame.js
+++ src/ConfigureGame/ConfigureGame.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ConfigureGame() {
+  const [wordsText, setWordsText] = useState('one\ntwo\nthree\nfour')
+  const words = wordsText.trim().split(/,?\s+/)
+  const searchString = `?w=${words.join('&w=')}`
+  const linkUrl = window.location.href + searchString
   return (
     <>
-      <h1>Add some words to the URL to play.</h1>
-      <p>For example:</p>
-      <p>
-        <a href={`${window.location.href}?w=one&w=two&w=three&w=four`}>
-          {window.location.href}?w=one&w=two&w=three&w=four
-        </a>
-      </p>
+      <h1>Word Memory</h1>
+      <label htmlFor="words">Create your list of words:</label>
+      <textarea
+        id="words"
+        value={wordsText}
+        onChange={(e) => setWordsText(e.target.value)}
+      />
+      {wordsText.trim().length ? (
+        <>
+          <p>Then click on the link to play:</p>
+          <p>
+            <a href={linkUrl}>{linkUrl}</a>
+          </p>
+        </>
+      ) : null}
     </>
   )
 }
