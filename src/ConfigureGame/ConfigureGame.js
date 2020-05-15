723e20426b7343c69649143a30ebcab48ac33f1d
Garrett Nay <g@garrettnay.com>
2020-05-15 14:26:41 -0600

    Allegedly add some styles

--- src/ConfigureGame/ConfigureGame.js
+++ src/ConfigureGame/ConfigureGame.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import styles from './ConfigureGame.module.css'
 
 function ConfigureGame() {
   const [wordsText, setWordsText] = useState('one\ntwo\nthree\nfour')
@@ -6,23 +7,28 @@ function ConfigureGame() {
   const searchString = `?w=${words.join('&w=')}`
   const linkUrl = window.location.href + searchString
   return (
-    <>
+    <div className={styles.container}>
       <h1>Word Memory</h1>
-      <label htmlFor="words">Create your list of words:</label>
+      <label htmlFor="words" className={styles.label}>
+        Create your list of words:
+      </label>
       <textarea
         id="words"
         value={wordsText}
         onChange={(e) => setWordsText(e.target.value)}
+        className={styles.textarea}
       />
       {wordsText.trim().length ? (
         <>
           <p>Then click on the link to play:</p>
           <p>
-            <a href={linkUrl}>{linkUrl}</a>
+            <a href={linkUrl} className={styles.link}>
+              {linkUrl}
+            </a>
           </p>
         </>
       ) : null}
-    </>
+    </div>
   )
 }
 
